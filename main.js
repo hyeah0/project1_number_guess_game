0d82f5410c67f070f2244170868e5bdcff807fb3
Jihye Kim <hye0ah0@gmail.com>
2022-01-21 01:18:20 +0900

    숫자 맞추기 게임 복습

---
+++ main.js
@@ -0,0 +1,14 @@
+// 1. 랜덤값
+// 2. 결과값 수정
+// 3. 1~100이 아닌수일경우 다시
+// 4. 기회는 5번
+
+let randomNumber = Math.floor(Math.random()*100)+1;
+console.log("정답은 : ", randomNumber);
+
+let userNumber = document.querySelector(".input");
+function result(){
+    if(userNumber == 0 || userNumber <= 100){
+        
+    }
+}
